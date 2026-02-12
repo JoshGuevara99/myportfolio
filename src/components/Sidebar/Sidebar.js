21e4d771d4250725e3bfc66dd76d72cf8dd71fe9
Joshua Guevara <joshguitar05@gmail.com>
2026-02-12 15:26:25 -0500

    updated linkedin

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,7 +4,7 @@ import {Link, NavLink} from 'react-router-dom'
 import logo from "../../assets/images/logo.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope,faHome, faUser } from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 export default function Sidebar() {
   return (
     <div className= 'nav-bar'>
@@ -28,19 +28,10 @@ export default function Sidebar() {
       <li>
         <a target = "_blank" 
         rel = "noreferrer"
-        href="https://www.linkedin.com/in/joshua-guevara-89b3b118b/">
+        href="https://www.linkedin.com/in/joshua-g-89b3b118b/">
           <FontAwesomeIcon icon = {faLinkedin} color = "#4d4d4e"/>
         </a>
       </li>
-      <li>
-        <a target = "_blank" 
-        rel = "noreferrer"
-        href="https://github.com/JoshGuevara99?tab=repositories">
-          <FontAwesomeIcon icon = {faGithub} color = "#4d4d4e"/>
-        </a>
-      </li>
-
-
     </ul>
       </div>
 
